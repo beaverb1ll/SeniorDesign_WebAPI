7c33b0d488d6dc2ffefc81fa0d110aedd6d6187d
Mike Tyrlik <tyrlikmike@gmail.com>
2013-09-30 20:55:13 -0400

    Works

--- orderDrink.php
+++ orderDrink.php
@@ -6,13 +6,13 @@ function getGUID(){
         mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
         $charid = strtoupper(md5(uniqid(rand(), true)));
         $hyphen = chr(45);// "-"
-        $uuid = chr(123)// "{"
-            .substr($charid, 0, 8).$hyphen
-            .substr($charid, 8, 4).$hyphen
-            .substr($charid,12, 4).$hyphen
-            .substr($charid,16, 4).$hyphen
+        $uuid = "1"// "{"
+            .substr($charid, 0, 8)
+            .substr($charid, 8, 4)
+            .substr($charid,12, 4)
+            .substr($charid,16, 4)
             .substr($charid,20,12)
-            .chr(125);// "}"
+            ."1";// "}"
         return $uuid;
     
 }
@@ -36,29 +36,29 @@ error_reporting(E_ALL);
 	$dbHost="localhost"; // Host name 
 	$dbUsername="root"; // Mysql username 
 	$dbPassword="password"; // Mysql password 
-	$dbName="new_schema"; // Database name 
+	$dbName="SD"; // Database name 
 	// $tbl_name="customers"; // Table name 
 
 	// Connect to server and select databse.
 	$dbCon = mysqli_connect("$dbHost", "$dbUsername", "$dbPassword", "$dbName");
 
 	if (mysqli_connect_errno($dbCon)) {
-    	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    	// echo "Failed to connect to MySQL: " . mysqli_connect_error();
     	returnFailedAllocate();
 	} 
 
 	// $columnName = mysqli_real_escape_string($columnName);
 	// $searchInput = mysqli_real_escape_string($searchInput);
 
-	// $query = "SELECT ing0, ing1, ing2, ing3, ing4, ing5 FROM orderTable WHERE orderID=\"0\"";
+	$query = "SELECT ing0, ing1, ing2, ing3, ing4, ing5 FROM orderTable WHERE orderID=\"0\"";
 
-	$result = mysqli_query($dbCon, "SELECT ing0, ing1, ing2, ing3, ing4, ing5 FROM orderTable WHERE orderID=\"0\"");
+	$result = mysqli_query($dbCon, $query);
 
 	$row = mysqli_fetch_array($result);
 
-	//if (!$row) {
-	// 	returnFailedAllocate();		
-	// }
+	if (!$row) {
+	 	returnFailedAllocate();		
+	}
 
 	$cIngred0 = intval($row['ing0']);
 	$cIngred1 = intval($row['ing1']);
@@ -67,10 +67,10 @@ error_reporting(E_ALL);
 	$cIngred4 = intval($row['ing4']);
 	$cIngred5 = intval($row['ing5']);
 
-	echo $cIngred0;
+	// echo $cIngred0;
 	if ($rIngred0 > 0) {
 		if ($cIngred0 - $rIngred0 < 0) {
-			echo "error ingredient";
+			// echo "error ingredient 0";
 			returnFailedAllocate();
 		}
 		$cIngred0 = $cIngred0 - $rIngred0;
@@ -78,6 +78,7 @@ error_reporting(E_ALL);
 
 	if ($rIngred0 > 1) {
 		if ($cIngred1 - $rIngred1 < 0) {
+			// echo "error ingredient 1";
 			returnFailedAllocate();
 		}
 		$cIngred1 = $cIngred1 - $rIngred1;
@@ -85,6 +86,7 @@ error_reporting(E_ALL);
 
 	if ($rIngred2 > 0) {
 		if ($cIngred2 - $rIngred2 < 0) {
+			// echo "error ingredient 2";
 			returnFailedAllocate();
 		}
 		$cIngred2 = $cIngred2 - $rIngred2;
@@ -92,6 +94,7 @@ error_reporting(E_ALL);
 
 	if ($rIngred3 > 0) {
 		if ($cIngred3 - $rIngred3 < 0) {
+			// echo "error ingredient 3";
 			returnFailedAllocate();
 		}
 		$cIngred3 = $cIngred3 - $rIngred3;
@@ -99,6 +102,7 @@ error_reporting(E_ALL);
 
 	if ($rIngred4 > 0) {
 		if ($cIngred4 - $rIngred4 < 0) {
+			// echo "error ingredient 4";
 			returnFailedAllocate();
 		}
 		$cIngred4 = $cIngred4 - $rIngred4;
@@ -106,35 +110,42 @@ error_reporting(E_ALL);
 
 	if ($rIngred5 > 0) {
 		if ($cIngred5 - $rIngred5 < 0) {
+			// echo "error ingredient 5";
 			returnFailedAllocate();
 		}
 		$cIngred5 = $cIngred5 - $rIngred5;
 	}
 
-	$query = "UPDATE orderTable SET ing0=" . $cIngred0 . ", ing1=" . $cIngred1 . ", ing2=" . $cIngred2 . ", ing3=" . $cIngred3 . ", ing4=" . $cIngred4 . ", ing5=" . $cIngred5 . "WHERE orderID=\"0\"";
-	
+	// echo "got past ingred compares\n";
 
-	// echo "DEBUG [searchDatabase::22]:" . $query . "<br>";
+	$query = "UPDATE orderTable SET ing0=" . $cIngred0 . ", ing1=" . $cIngred1 . ", ing2=" . $cIngred2 . ", ing3=" . $cIngred3 . ", ing4=" . $cIngred4 . ", ing5=" . $cIngred5 . " WHERE orderID=\"0\"";
 	
-	$result = $dbCon->query($query);
+	// echo $query;
+	$result = mysqli_query($dbCon, $query);
 
 	if (!$result) {
+		// echo "Error updating reserved ingredients\n";
+		// echo mysqli_error($dbCon);
 		returnFailedAllocate();
 	}
 
+	
+	date_default_timezone_set("America/New_York");
 	$date = date_create();
 	$time = date_timestamp_get($date);
 	$barcode = getGUID();
 
-	$query = 'INSERT INTO orderTable (orderID, ing0, ing1, ing2, ing3, ing4, ing5, orderTime) VALUES (' . $barcode.', '.$rIngred0.', '.$rIngred1.', '.$rIngred2.', '.$rIngred3.', '.$rIngred4.', '.$rIngred5.', '.$time.')';
-
-	$result = $dbCon->query($query);
+	$query = 'INSERT INTO orderTable (orderID, ing0, ing1, ing2, ing3, ing4, ing5, orderTime) VALUES ("' . $barcode.'", '.$rIngred0.', '.$rIngred1.', '.$rIngred2.', '.$rIngred3.', '.$rIngred4.', '.$rIngred5.', '.$time.')';
+	// echo $query;
+	$result = mysqli_query($dbCon, $query);
 
 	if (!$result) {
+		// echo "Error inserting new drink order\n";
+		// echo mysqli_error($dbCon);
 		returnFailedAllocate();
 	}
 
-	echo "{\"barcode\":\"".$barcode."\"}";
+	echo "\n{\"barcode\":\"".$barcode."\"}\n";
 
 
 ?>
