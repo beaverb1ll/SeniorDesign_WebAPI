75fa227110d6b18179fc3e91925039042ba7cd1a
Mike <tyrlikmike@gmail.com>
2013-10-02 23:10:09 -0400

    Fixed SQL Update error

--- cancelOrder.php
+++ cancelOrder.php
@@ -2,66 +2,70 @@
 
 function returnFailed(){
 // return 0 for failed.  
-	$arr = array ('success'=>0);
-    echo json_encode($arr);
-	die();
+        $arr = array ('success'=>0);
+        echo json_encode($arr);
+        die();
 
 }
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 
-	if (!isset($_POST["barcode"])) {
-		returnFailedAllocate();
-	}
+        if (!isset($_POST["barcode"])) {
+                returnFailedAllocate();
+        }
 
-	$barcode = $_POST["barcode"];
-
+        $barcode = $_POST["barcode"];
+        // echo $barcode;
 // $barcode = "15BFB983B4D9DF5AE8C9B0F0786ACACCD1";
 
 
 
-	$dbHost="localhost"; // Host name 
-	$dbUsername="root"; // Mysql username 
-	$dbPassword="password"; // Mysql password 
-	$dbName="SD"; // Database name 
+        $dbHost="localhost"; // Host name 
+        $dbUsername="root"; // Mysql username 
+        $dbPassword="password"; // Mysql password 
+        $dbName="SD"; // Database name 
 
-	// Connect to server and select databse.
-	$dbCon = mysqli_connect("$dbHost", "$dbUsername", "$dbPassword", "$dbName");
+        // Connect to server and select databse.
+        $dbCon = mysqli_connect("$dbHost", "$dbUsername", "$dbPassword", "$dbName");
 
-	if (mysqli_connect_errno($dbCon)) {
-    	// echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    	returnFailed();
-	} 
+        if (mysqli_connect_errno($dbCon)) {
+                // echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                returnFailed();
+        } 
 
-	if (!mysqli_set_charset($dbCon, "utf8")) {
-    	returnFailedAllocate();
-	}
+        if (!mysqli_set_charset($dbCon, "utf8")) {
+                // echo "Failed to set charset";
+                returnFailedAllocate();
+        }
 
-	// select orderTIme and subtract 600s (10 Mins) update orderTime
-	$query = "SELECT orderTime FROM orderTable WHERE orderID=\"" .$barcode . "\"";
+        // select orderTIme and subtract 600s (10 Mins) update orderTime
+        $query = "SELECT orderTime FROM orderTable WHERE orderID=\"" .$barcode . "\"";
 
-	$result = mysqli_query($dbCon, $query);
+        $result = mysqli_query($dbCon, $query);
 
-	$row = mysqli_fetch_array($result);
+        $row = mysqli_fetch_array($result);
 
-	if (!$row) {
-	 	returnFailed();		
-	}
+        if (!$row) {
+                // echo "unable to fetch ordertime";
+                returnFailed();
+        }
 
-	$orderTime = intval($row['orderTime']);
-	$orderTime = $orderTime - 600;
+        $orderTime = intval($row['orderTime']);
+        $orderTime = $orderTime - 600;
 
-	$query = 'UPDATE orderTable SET orderTime="' . $orderTime . '" WHERE orderID="' . $barcode .'"';
-	
-	// $safeQuery = mysqli_real_escape_string($dbCon, $query);
-	$result = mysqli_query($dbCon, $query);
+        $query = 'UPDATE orderTable SET orderTime="' . $orderTime . '" WHERE orderID="' . $barcode .'"';
 
-	$row = mysqli_fetch_array($result);
-	if (!$row) {
-	 	returnFailed();		
-	}
+        // echo $query;
+        // $safeQuery = mysqli_real_escape_string($dbCon, $query);
+        $result = mysqli_query($dbCon, $query);
 
-	$arr = array ('success'=>1);
-    echo json_encode($arr);
+        // $row = mysqli_fetch_array($result);
+        if (!$result) {
+                // echo "unable to update orderTime";
+                returnFailed();
+        }
+
+        $arr = array ('success'=>1);
+        echo json_encode($arr);
 ?>
