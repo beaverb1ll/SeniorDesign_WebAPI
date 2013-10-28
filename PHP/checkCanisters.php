80dd5e6ee605010ecea2529fec179cdc71706095
Mike <tyrlikmike@gmail.com>
2013-10-28 18:36:23 -0400

    Added Check Canister ability

---
+++ PHP/checkCanisters.php
@@ -0,0 +1,47 @@
+<?php
+
+function returnFailedAllocate(){
+	$arr = array ('barcode'=>-1);
+    echo json_encode($arr);
+	die();
+
+}
+
+// ini_set('display_errors', 'On');
+// error_reporting(E_ALL);
+
+
+	$dbHost="localhost"; // Host name 
+	$dbUsername="root"; // Mysql username 
+	$dbPassword="password"; // Mysql password 
+	$dbName="SD"; // Database name 
+
+
+	
+
+	// Connect to server and select databse.
+	$dbCon = mysqli_connect("$dbHost", "$dbUsername", "$dbPassword", "$dbName");
+	if (mysqli_connect_errno($dbCon)) {
+    	// echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    	returnFailedAllocate();
+	} 
+
+	if (!mysqli_set_charset($dbCon, "utf8")) {
+		// echo 'unable to change charset';
+    	returnFailedAllocate();
+	}
+
+	// $columnName = mysqli_real_escape_string($columnName);
+	// $searchInput = mysqli_real_escape_string($searchInput);
+
+	$query = "SELECT ing0, ing1, ing2, ing3, ing4, ing5 FROM orderTable WHERE orderID=\"0\"";
+	$result = mysqli_query($dbCon, $query);
+	$row = mysqli_fetch_array($result);
+	if (!$row) {
+		// echo 'unable to fetch row';
+	 	returnFailedAllocate();		
+	}
+
+    echo json_encode($row);
+
+?>
