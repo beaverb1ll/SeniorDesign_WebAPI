8290f0b107ba4ab55057262a03fabdda2fa6857c
Mike <tyrlikmike@gmail.com>
2013-11-06 20:34:09 -0500

    added unreserved and remaining logic

--- PHP/checkCanisters.php
+++ PHP/checkCanisters.php
@@ -34,12 +34,32 @@ function returnFailedAllocate(){
 
 	$query = "SELECT ing0, ing1, ing2, ing3, ing4, ing5 FROM orderTable WHERE orderID=\"0\"";
 	$result = mysqli_query($dbCon, $query);
-	$row = mysqli_fetch_array($result, MYSQLI_NUM);
-	if (!$row) {
+	$unreserved_row = mysqli_fetch_array($result, MYSQLI_NUM);
+	if (!$unreserved_row) {
 		// echo 'unable to fetch row';
 	 	returnFailedAllocate();		
 	}
 
-    echo json_encode($row);
+	// remaining is the addition of non picked up and non expired drinks to unreserved.
+	$query = 'SELECT SUM(Ing0), SUM(Ing1), SUM(Ing2), SUM(Ing3), SUM(Ing4), SUM(Ing5) from orderTable WHERE expired="false" AND pickedUp="false"';
+	$result = mysqli_query($dbCon, $query);
+	$remaining_row = mysqli_fetch_array($result, MYSQLI_NUM);
+	if (!$remaining_row) {
+		// echo 'unable to fetch row';
+	 	returnFailedAllocate();		
+	}
+
+	$remaining_row[0] += $unreserved_row[0];
+	$remaining_row[1] += $unreserved_row[1];
+	$remaining_row[2] += $unreserved_row[2];
+	$remaining_row[3] += $unreserved_row[3];
+	$remaining_row[4] += $unreserved_row[4];
+	$remaining_row[5] += $unreserved_row[5];
+
+	echo '{"remaining": ["'
+    echo json_encode($remaining_row);
+    echo '],"unreserved": ['
+    echo json_encode($unreserved_row);
+    echo ']}'
 
 ?>
